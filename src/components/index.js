079cff4798ab44dbe4e8814c19373ea6a1548707
valdir-ti <valdir.ti@gmail.com>
2020-03-11 17:03:25 -0300

    Ajustando a questão do tempo de loading da página

--- src/components/index.js
+++ src/components/index.js
@@ -7,12 +7,10 @@ const asyncFunc = () => {
   return new Promise(resolve => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(location => {
-        setTimeout(() => resolve(location.coords), 600);
+        setTimeout(() => resolve(location.coords), 500);
       });
     } else {
-      alert(
-        "I'm sorry, but geolocation services are not supported by your browser."
-      );
+      alert("I'm sorry, but geolocation services are not supported by your browser.");
     }
   });
 };
@@ -31,26 +29,25 @@ export default class Wheater extends Component {
       calculada: false
     };
   }
-
-  UNSAFE_componentWillMount = async () => {
+  
+  componentDidMount = async () => {
+    
     const location = await asyncFunc();
     this.setState({ location: location });
 
-    const proxy = "https://cors-anywhere.herokuapp.com/";
-    let myKey = "a1fd0337efdabfc77432bb7566663861";
-    const api = `${proxy}https://api.darksky.net/forecast/${myKey}/${this.state.location.latitude},${this.state.location.longitude}`;
+    const proxy    = "https://cors-anywhere.herokuapp.com/";
+    let myKey      = "a1fd0337efdabfc77432bb7566663861";
+    const api      = `${proxy}https://api.darksky.net/forecast/${myKey}/${this.state.location.latitude},${this.state.location.longitude}`;
     const response = await axios.get(api);
-    // console.log(response.data);
+    
     this.setState({
       locate: response.data,
       temperature: response.data.currently,
       _tempInitial: response.data.currently.temperature,
-      _temp: response.data.currently.temperature
+      _temp: response.data.currently.temperature,
+      loading: false
     });
-  };
 
-  componentDidMount = async () => {
-    this.state.loading = false;
   };
 
   handleClick() {
@@ -84,11 +81,7 @@ export default class Wheater extends Component {
           <h1 className="timezone-location">{this.state.locate.timezone}</h1>
           <WeatherIcon
             name="darksky"
-            iconId={
-              this.state.temperature.icon
-                ? this.state.temperature.icon
-                : "clear-day"
-            }
+            iconId={this.state.temperature.icon}
             flip="horizontal"
             rotate="90"
           />
